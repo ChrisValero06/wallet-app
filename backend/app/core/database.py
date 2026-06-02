b4e5a338cd53cf3761a91ee3f419f2d0eb706808
unknown <christophervalero05@hotmail.com>
2026-06-02 14:44:27 -0600

    fix: se cambio la version de python 3.12 y psycopg2-binary para que funcionen en render

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -2,15 +2,11 @@ from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from app.core.config import settings
 
-db_url = settings.DATABASE_URL
-if db_url.startswith("postgresql://"):
-    db_url = db_url.replace("postgresql://", "postgresql+psycopg://", 1)
+connect_args = {"check_same_thread": False} if settings.DATABASE_URL.startswith("sqlite") else {}
 
-connect_args = {"check_same_thread": False} if db_url.startswith("sqlite") else {}
+engine = create_engine(settings.DATABASE_URL, connect_args=connect_args)
 
-engine = create_engine(db_url, connect_args=connect_args)
-
-if db_url.startswith("sqlite"):
+if settings.DATABASE_URL.startswith("sqlite"):
     @event.listens_for(engine, "connect")
     def set_sqlite_pragma(dbapi_connection, connection_record):
         cursor = dbapi_connection.cursor()
